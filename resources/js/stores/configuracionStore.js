d61cb6ba6ca555d6d27cde103546d9dc8cf7a5aa
dscater <victorgonzalo_as@hotmail.com>
2024-11-09 15:29:35 -0400

    Subida proyecto

---
+++ resources/js/stores/configuracionStore.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+
+export const useConfiguracionStore = defineStore("configuracion", {
+    state: () => ({
+        oConfiguracion: {
+            nombre_sistema:"LOGICORP",
+            alias:"LC",
+            razon_social:"LOGICORP S.A.",
+            ciudad:"",
+            dir:"",
+            fono:"",
+            correo:"",
+            web:"",
+            actividad:"",
+            // appends
+            url_logo: "",
+        },
+    }),
+    actions: {
+        setInstiticion(value) {
+            this.oConfiguracion = value;
+        },
+    },
+});
